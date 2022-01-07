81f815c990a17140d8c7a21bb4be2d1feb8a421e
zillerium <sales@merrows.co.uk>
2022-01-07 18:24:40 +0000

    samples

---
+++ rust1/hashset.rs
@@ -0,0 +1,26 @@
+use std::thread;
+use std::time;
+use std::collections::HashSet;
+
+fn main() {
+
+        let mut greeks = HashSet::new();
+        greeks.insert("gamma");
+        greeks.insert("gamma2");
+        greeks.remove("gamma2");
+        if greeks.insert("gamm88a") {
+            println!("{:?}", greeks);
+        }
+
+    let _1_5: HashSet<_> = (1..=5).collect();
+    let _6_10: HashSet<_> = (6..=10).collect();
+    let _1_10: HashSet<_> = (1..=10).collect();
+    let _2_8: HashSet<_> = (2..=8).collect();
+
+    println!("is {:?} a subset of {:?}? {}", _2_8, _1_10, _2_8.is_subset(&_1_10)); 
+    println!("is {:?} a subset of {:?}? {}", _2_8, _1_10, _2_8.is_disjoint(&_1_10)); 
+    println!("is {:?} a subset of {:?}? {:?}", _2_8, _1_10, _2_8.union(&_1_10)); 
+
+    
+
+}
